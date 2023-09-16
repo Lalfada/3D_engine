72d3d0b6670fa096d9fc9ab23206c6a97259e77b
Lalfada <bgolovtchiner@icloud.com>
2023-09-16 15:45:41 +0200

    .obj support and painter algorithme
    Allows the importation .obj file if they only have triangular faces.
    Also implements the painter's algorithm in order to draw the object nearest to the camera. However that algorithm do have some issues and will most likely be changed.

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def get_yrotation_matrix(angle):
 cube_verticies = [Vec3(0, 0, 0), Vec3(1, 0, 0), Vec3(1, 1, 0), Vec3(0, 1, 0),
     Vec3(0, 0, 1), Vec3(1, 0, 1), Vec3(1, 1, 1), Vec3(0, 1, 1)]
 
-mesh_cube_indices = [
+cube_indices = [
     # south
     [0, 3, 2],
     [2, 1, 0],    
@@ -71,12 +71,41 @@ mesh_cube_indices = [
     [5, 4, 0],
 ]
 
-mesh_cube = []
-for row in mesh_cube_indices:
-    newRow = []
-    for k in row:
-        newRow.append(deepcopy(cube_verticies[k]))
-    mesh_cube.append(newRow)
+def mesh_from_indices(vertices, indices):
+    mesh = []
+    for row in indices:
+        newRow = []
+        for k in row:
+            newRow.append(deepcopy(vertices[k]))
+        mesh.append(newRow)
+    return mesh
+
+
+# thx chat gpt
+def meshdata_from_lines(lines):
+    vertices, indices = [], []
+    for line in lines:
+            parts = line.strip().split()
+            if not parts:
+                continue
+            if parts[0] == 'v':
+                # Parse vertex (v) lines
+                vertex = Vec3(float(parts[1]), float(parts[2]), float(parts[3]))
+                vertices.append(vertex)
+            elif parts[0] == 'f':
+                # Parse face (f) lines
+                tri = [int(part.split('/')[0]) - 1 for part in parts[1:]]  # Subtract 1 to convert to 0-based indexing
+                indices.append(tri)
+    return vertices, indices
+
+
+# thx chat gpt
+def mesh_from_obj(path):
+    with open(path, 'r') as obj_file:
+        lines = obj_file.readlines()
+        vertices, indices = meshdata_from_lines(lines)
+        
+        return mesh_from_indices(vertices, indices)
 
 
 def get_normal(tri):
@@ -84,7 +113,7 @@ def get_normal(tri):
     B = tri[2] - tri[0]
     return A.cross(B)
 
-
+    
 def luminosity_from_light(normal, light):
     luminosity = -normal.dot(light)
     luminosity *= 255 / normal.length()
@@ -113,19 +142,37 @@ def update():
     theta_y =  theta_x * 0.5
     xrot_mat = get_xrotation_matrix(theta_x)
     yrot_mat = get_yrotation_matrix(theta_y)
-    mesh_to_compute = deepcopy(mesh_cube)
+    mesh_to_compute = deepcopy(model_mesh)
+
+    drawing_buffer = []
 
     for tri in mesh_to_compute:
+        # rotate and translate
         for i, vec in enumerate(tri):
             vec = vec.matrix_mul(xrot_mat)
             vec = vec.matrix_mul(yrot_mat)
-            vec.z += 3.0
+            vec.z += 10.0
+
             tri[i] = vec
 
+        # only draw triangles facing towards the camera
         tri_normal = get_normal(tri)
         if tri[0].dot(tri_normal) >= 0:
-            continue       
+            continue   
 
+        z_mid = (tri[0].z + tri[1].z + tri[2].z) / 3.0
+        drawing_buffer.append((tri, z_mid, tri_normal))
+
+    # sort based on the distance to the camera
+    # this is an implementation of the painter's algorithm
+    sorted_drawing_buffer = sorted(drawing_buffer, reverse = True, key = 
+        lambda v: v[1]
+        )
+
+    for v in sorted_drawing_buffer:
+        (tri, _, tri_normal) = v
+
+        # projection and screen scaling
         for i, vec in enumerate(tri):
             vec = vec.extended_matrix_mul(PROJECTION_MATRIX)
             vec += Vec3(1, 1, 0)
@@ -161,8 +208,15 @@ def game_loop():
 
 
 if __name__ == "__main__":
+    # mesh_cube = mesh_from_indices(cube_verticies, cube_indices) 
+    model_path = "AirShip.obj"
+    model_mesh = mesh_from_obj(model_path)
+
+    # initial setup
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+    # core loop
     game_loop()
+    # quit
     pygame.quit()
